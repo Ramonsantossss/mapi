9cef17010255a7d44606bba421516a323279b13f
Ramonsantosss <103340294+Ramonsantossss@users.noreply.github.com>
2023-09-06 10:03:47 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const api = require("./api");
 const app = express();
 const port = 8080;
-const cors = require('cors');
+//const cors = require('cors');
 
-app.use(cors());
+//app.use(cors());
 app.set("views", "./static/");
 app.use(express.static("./public/"))
 app.engine('html', require('ejs').renderFile);
